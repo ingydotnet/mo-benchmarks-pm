59b7338520bdcc8ed03a78190cea7a047cec99ea
Ingy dot Net <ingy@ingy.net>
2011-10-03 02:02:22 +0100

    Released version 0.10

--- lib/Mo/Benchmarks.pm
+++ lib/Mo/Benchmarks.pm
@@ -44,23 +44,64 @@ use Module::Pluggable
 Mo::Benchmarks->plugins;
 
 #------------------------------------------------------------------------------#
-package Mo::Benchmarks::Command::xxx;
+package Mo::Benchmarks::Command::constructor;
 Mo::Benchmarks->import( -command );
 use Mouse;
 extends 'Mo::Benchmarks::Command';
 
-use constant abstract => '...';
-use constant usage_desc => 'mo-benchmarks xxx ...';
+use Benchmark ':all';
 
-has yyy => (
+use constant abstract => 'Run constructor benchmarks';
+use constant usage_desc =>
+    'mo-benchmarks constructor --count=1000000 Moose Mouse Moo Mo';
+
+has count => (
     is => 'ro',
-    isa => 'Str',
-    documentation => '...',
+    isa => 'Num',
+    documentation => 'Number of times to run a test',
 );
 
 sub execute {
     my ($self, $opt, $args) = @_;
-    ...    
+    my @mo = map lc, grep !/^--/, @$args;
+    @mo = qw'mo moo mouse moose' unless @mo;
+    my $tests = {
+        map {
+            my $t = $_;
+            my $l = lc($t);
+            my $m =
+            eval <<"...";
+package $l;
+use $t;
+has good => (is => 'ro');
+has bad => (is => 'ro');
+has ugly => (is => 'rw');
+$l->new(good => 'Perl', bad => 'Python', ugly => 'Ruby');
+...
+            my $v = do { no strict 'refs'; ${$t."::VERSION"} };
+            ($l => [ "$t $v" =>
+                sub {
+#                     my $m =
+                    $l->new(good => 'Perl', bad => 'Python', ugly => 'Ruby');
+#                     $m->good;
+#                     $m->bad;
+#                     $m->ugly;
+#                     $m->ugly('Bunny');
+                }
+            ])
+        } qw(Mo Moo Mouse Moose)
+    };
+
+    my $count = $self->count || 1000;
+    my $num = 1;
+    timethese($count, {
+        map {
+            (
+                $num++ . ") $_->[0]",
+                $_->[1]
+            )
+        } map $tests->{$_}, @mo
+    });
 }
 
 #------------------------------------------------------------------------------#
